608b643f0778a69c567edaa55fb949a30f498c68
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2024-12-01 13:43:26 -0500

    Update README.md and script.sh

--- script.sh
+++ script.sh
@@ -7,4 +7,19 @@ touch input.txt
 cp ../day.py .
 sed -i '' '1,2d' day.py
 mv day.py day-0$1.py
+touch README.md
+cat <<EOL > README.md
+# Day-0$1
+
+> https://adventofcode.com/2024/day/$1
+
+---
+
+## Answers
+
+- Part 1: 
+- Part 2: 
+
+## Notes
+EOL
 echo "ready for day-0$1"
